0201c98b121bd331e115580cad80cb46ae85c6e8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-09-02 18:09:10 +0200

    [UPD] Snack 1 DONE

--- snacks.test.js
+++ snacks.test.js
@@ -1,6 +1,9 @@
-const { } = require("./snacks.js")
+const { getInitials } = require("./snacks.js")
 
 //Snack 1
-test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
 
+test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
+    expect(getInitials("Mario Rossi")).toBe("M.R.");
+    expect(getInitials("luigi bianchi")).toBe("L.B.");
+    expect(getInitials("Maria Verdi")).toBe("M.V.");
 });
