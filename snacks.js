bdbe645284b9f16f84325f8a92e8b6d8e2b684cb
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-09-03 10:12:50 +0200

    [UPD] Snack 7 DONE

--- snacks.js
+++ snacks.js
@@ -24,9 +24,26 @@ function isPalindrome(parola) {
     return parola.trim() === parolaInversa;
 }
 
+function findPostById(posts, id) {
+    if (isNaN(id)) {
+        throw new Error(`"${id}" non è un id`);
+    }
+    posts.forEach(p => {
+        if (
+            p.id === undefined ||
+            p.title === undefined ||
+            p.slug === undefined
+        ) {
+            throw new Error('Questo array posts non è nel formato corretto');
+        }
+    })
+    return posts.find(p => p.id === id) || null;
+}
+
 module.exports = {
     getInitials,
     createSlug,
     average,
-    isPalindrome
+    isPalindrome,
+    findPostById
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,4 +1,4 @@
-const { getInitials, createSlug, average, isPalindrome } = require("./snacks.js")
+const { getInitials, createSlug, average, isPalindrome, findPostById } = require("./snacks.js")
 
 //Snack 1
 test('La funzione getInitials restituisce le iniziali di un nome completo.', () => {
@@ -33,4 +33,17 @@ test('La funzione isPalindrome verifica se una stringa è un palindromo.', () =>
 test('La funzione createSlug lancia un errore se il titolo è vuoto o non valido.', () => {
     expect(() => createSlug("")).toThrow('Titolo non valido');
     expect(() => createSlug(null)).toThrow('Titolo non valido');
+});
+
+const posts = [
+    { id: 1, title: "Introduzione a Javascript", slug: "introduzione-a-javascript" },
+    { id: 2, title: "React Hooks", slug: "react-hooks" }
+];
+
+//Snack 7
+test(`La funzione findPostById restituisce il post corretto dato l'array di post e l'id`, () => {
+    expect(findPostById(posts, 2)).toEqual({ id: 2, title: "React Hooks", slug: "react-hooks" });
+    expect(findPostById(posts, 3)).toEqual(null);
+    expect(() => findPostById(posts, "ciao")).toThrow('"ciao" non è un id');
+    expect(() => findPostById([34, 67], 2)).toThrow('Questo array posts non è nel formato corretto');
 });
